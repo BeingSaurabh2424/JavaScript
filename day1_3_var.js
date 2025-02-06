76a2debfca8d4c79f9c531c15f447426630d94be
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-02-06 22:02:36 +0530

    Update day1_3_var.js

---
+++ day1_3_var.js
@@ -0,0 +1,10 @@
+  console.log("Var vs Let vs Const");
+  var a = 45;
+  var b = "Saurabh";
+  var c = null;
+  var d = undefined
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
