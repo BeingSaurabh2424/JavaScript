3fcbd8bc8ea9275a215bffdeff1fcca11276549f
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-02-18 22:48:07 +0530

    Create day9_3.js
    Random Color Generator

---
+++ day9_3.js
@@ -0,0 +1,11 @@
+//Random Color Generator
+const randomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  console.log(randomColor()); // Output: Random Hex Color Code
+  
